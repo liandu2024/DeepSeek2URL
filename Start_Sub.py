11eafc9d72fdebb76794dde7af1b2077b2eabfa3
安格视界 <155928686+liandu2024@users.noreply.github.com>
2025-02-10 22:56:38 +0800

    Update Start_Sub.py

--- Start_Sub.py
+++ Start_Sub.py
@@ -1,8 +1,8 @@
 import subprocess
 
-subprocess.Popen(["python", "D:/Python\DS2URL/Ark.py"])
-subprocess.Popen(["python", "D:/Python\DS2URL/DS.py"])
-subprocess.Popen(["python", "D:/Python\DS2URL/SF.py"])
-subprocess.Popen(["python", "D:/Python\DS2URL/NV.py"])
-subprocess.Popen(["python", "D:/Python\DS2URL/AL.py"])
+subprocess.Popen(["python", "D:/Python/DS2URL/Ark.py"])
+subprocess.Popen(["python", "D:/Python/DS2URL/DS.py"])
+subprocess.Popen(["python", "D:/Python/DS2URL/SF.py"])
+subprocess.Popen(["python", "D:/Python/DS2URL/NV.py"])
+subprocess.Popen(["python", "D:/Python/DS2URL/AL.py"])
 
